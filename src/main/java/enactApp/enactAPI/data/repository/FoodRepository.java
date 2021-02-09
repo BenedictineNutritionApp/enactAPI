1c80561b9d0d4242b3025e6ac3c15f464f0264dc
AnthonyKuczwara <63365651+AnthonyKuczwara@users.noreply.github.com>
2021-02-09 11:06:27 -0600

    Revert "added ssl key"

--- src/main/java/enactApp/enactAPI/data/repository/FoodRepository.java
+++ src/main/java/enactApp/enactAPI/data/repository/FoodRepository.java
@@ -3,16 +3,12 @@ package enactApp.enactAPI.data.repository;
 import enactApp.enactAPI.data.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
  * Repository for Food
  */
 public interface FoodRepository extends JpaRepository<Food, Long> {
-
-    Optional<Food> findFoodByKeylist(String keylist);
-    List<Food> findAll();
 
 
 
