454878ffd21157580d5ea13f0b4a0fd5bcd3bd2f
BenedictineHealthApp <benedictinenutritionapp@gmail.com>
2021-02-09 12:45:12 -0600

    added login endpoints

--- src/main/java/enactApp/enactAPI/web/controller/UserController.java
+++ src/main/java/enactApp/enactAPI/web/controller/UserController.java
@@ -1,16 +1,13 @@
 package enactApp.enactAPI.web.controller;
 
 import enactApp.enactAPI.data.model.User;
-import enactApp.enactAPI.data.model.UserLogin;
 import enactApp.enactAPI.data.repository.UserRepository;
-import enactApp.enactAPI.data.translator.UserTranslator;
-import enactApp.enactAPI.web.models.UserView;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Date;
 import java.util.Optional;
 
 
@@ -28,98 +25,57 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    /**
-     * Finds and returns the user's view
-     *
-     * @param email The users email
-     * @param password The users password
-     * @return The user view
-     * @throws Exception
-     */
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
-    @GetMapping(value = "/api/users/{email}/{password}")
-    public UserView viewUser(@PathVariable String email, @PathVariable String password) throws Exception {
-        Optional<User> optionalUser = userRepository.findByUserEmail(email);
-        if (optionalUser.isEmpty()) {
-            log.error("User Email: [{}] does not exist.", email);
-            throw new Exception("User Email: " + email + " does not exist.");
-        }
-        return UserTranslator.entityToView(optionalUser.get());
-    }
-
-    /**
-     * This method is responsible for logging the user into their account
-     *
-     * @param userLogin The userLogin
-     * @return Dsiplay message
-     */
-    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
-    @PostMapping(value = "api/users/login")
-    public String login(@Valid @RequestBody UserLogin userLogin) {
-        //Checks that the entered in information is not empty
-        String email = userLogin.getEmail();
-        String password = userLogin.getPassword();
-        if (email.equalsIgnoreCase("") || email.equalsIgnoreCase(" ") || password.equalsIgnoreCase("") || password.equalsIgnoreCase(" ")) {
-            return "Invalid";
-        }
-        // If the user does not exist currently
-        Optional<User> optionalUser = userRepository.findByUserEmail(email);
-        if (optionalUser.isEmpty()) {
-            return "Invalid";
-        }
-
-        User user = optionalUser.get();
-        //Checks that the entered information is correct against that user's stored information
-        String hashedStoredPass = user.getUserPassword();
-        String hashedEnteredPass = encode(password);
-        System.out.println("stored pass " + hashedStoredPass);
-        System.out.println("entered pass " + hashedEnteredPass);
-        //If it is incorrect
-        if (!hashedEnteredPass.equals(hashedStoredPass)) {
-            return "Invalid";
-        }
-        return "Welcome " + user.getUserEmail() + "!";
-    }
-
-    /**
-     * Encodes the user's password
-     *
-     * @param password The password being encoded
-     * @return The encoded password
-     */
-    @SneakyThrows
-    public String encode(String password) {
-        String encodedPass = "";
-        for (int i = 0; i < password.length(); i++) {
-            char x = password.charAt(i);
-            x += 5;
-            encodedPass += x;
-        }
-
-//        SecureRandom random = new SecureRandom();
-//        byte[] salt = new byte[16];
-//        random.nextBytes(salt);
-//        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-//        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-//        byte[] hash = factory.generateSecret(spec).getEncoded();
-//        String encodedPass = "";
-//        for (int i = 0; i < hash.length; i++) {
-//            encodedPass+=hash[i];
-//        }
-        return encodedPass;
-    }
 
     @CrossOrigin(origins = "http://localhost:4200/", allowedHeaders = "*")
     @GetMapping(value = "api/users/checkIfEmailExists/{email}")
     public boolean checkIfEmailExists(@PathVariable String email) {
         // If the user does not exist currently
-        Optional<User> optionalUser = userRepository.findByUserEmail(email);
-        if (optionalUser.isPresent()) {
-            return true;
-        }else{
-            return false;
-        }
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+        return optionalUser.isPresent();
 
+    }
+
+    /**
+     * This method creates the user by setting the user details and saving them to the database, returning error messages or success message when applicable.
+     *
+     * @param user The user
+     * @return The string containing the error type or successful registration pop up
+     */
+    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+    @PostMapping(value = "api/users/register")
+    public String createUser(@Valid @RequestBody User user) {
+        //Checks if the entered email is already in use
+        Optional<User> optionalUser = userRepository.findUserByEmail(user.getEmail());
+        if (optionalUser.isPresent()) {
+            return "Username already taken";
+        }
+        //Sets the user's details and saves them to the database
+//        user.setPassword(encode(user.getPassword()));
+        user.setPassword(user.getPassword());
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        userRepository.save(user);
+        System.out.println(user.getEmail());
+        return "Registration Successful. You have been logged in.";
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+    @GetMapping(value = "api/users/login/{email}/{password}")
+    public String login(@PathVariable String email, @PathVariable String password) {
+        // If the user does not exist currently
+        Optional<User> optionalUser = userRepository.findUserByEmail(email);
+        if (optionalUser.isEmpty()) {
+            return "invalid";
+        }
+        User user = optionalUser.get();
+        //Checks that the entered information is correct against that user's stored information
+        String storedPass = user.getPassword();
+        //If it is incorrect
+        if (!password.equals(storedPass)) {
+            return "invalid";
+        }
+        return "valid";
     }
 
 
