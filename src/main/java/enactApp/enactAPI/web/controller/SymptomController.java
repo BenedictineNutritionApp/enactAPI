dca4977de9350d353efc4cf0eed645fa7b5ded0c
Brendamus <50035059+Brendamus@users.noreply.github.com>
2021-03-02 11:59:31 -0600

    symptom backend

---
+++ src/main/java/enactApp/enactAPI/web/controller/SymptomController.java
@@ -0,0 +1,53 @@
+package enactApp.enactAPI.web.controller;
+
+import enactApp.enactAPI.data.model.FitnessActivity;
+import enactApp.enactAPI.data.model.Food;
+import enactApp.enactAPI.data.model.Symptom;
+import enactApp.enactAPI.data.model.User;
+import enactApp.enactAPI.data.repository.FitnessActivityRepository;
+import enactApp.enactAPI.data.repository.SymptomRepository;
+import enactApp.enactAPI.data.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class SymptomController {
+    @Autowired
+    private final SymptomRepository symptomRepository;
+
+    public SymptomController(SymptomRepository symptomRepository) {
+        this.symptomRepository = symptomRepository;
+    }
+    @GetMapping(value = "/api/symptom/all")
+    public List<Symptom> getAllSymptom() {
+        List<Symptom> symptomList = symptomRepository.findAll();
+        return symptomList;
+    }
+
+    @PostMapping(path = "/api/symptom/add/")
+    public boolean saveSymptom(@RequestBody Symptom symptom){
+        Symptom newSymptom = new Symptom();
+        newSymptom.setAbdominalPain(symptom.isAbdominalPain());
+        newSymptom.setAppetiteLoss(symptom.isAppetiteLoss());
+        newSymptom.setBloating(symptom.isBloating());
+        newSymptom.setConstipation(symptom.isConstipation());
+        newSymptom.setDiarrhea(symptom.isDiarrhea());
+        newSymptom.setNausea(symptom.isNausea());
+        newSymptom.setStomaProblems(symptom.isStomaProblems());
+        newSymptom.setVomiting(symptom.isVomiting());
+        newSymptom.setOther(symptom.getOther());
+        newSymptom.setDateTime(symptom.getDateTime());
+        newSymptom.setUpdated(new Date());
+        newSymptom.setCreated(new Date());
+        symptomRepository.save(newSymptom);
+        System.out.println(symptom.getOther());
+        return true;
+    }
+
+}
