a37b3b5797cc70b568ec13dc85c59a8218cba4c4
BenedictineHealthApp <benedictinenutritionapp@gmail.com>
2021-04-11 23:54:37 -0500

    admin_site_crud

--- src/main/java/enactApp/enactAPI/data/repository/WeeklyGoalsRepository.java
+++ src/main/java/enactApp/enactAPI/data/repository/WeeklyGoalsRepository.java
@@ -4,8 +4,11 @@ import enactApp.enactAPI.data.model.WeeklyGoals;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WeeklyGoalsRepository extends JpaRepository<WeeklyGoals, Long> {
 
     List<WeeklyGoals> findAll();
+    Optional<WeeklyGoals> findWeeklyGoalsByGoalDescription(String description);
+    Optional<WeeklyGoals> findWeeklyGoalsById(Long Id);
 }
--- src/main/java/enactApp/enactAPI/data/translator/UserTranslator.java
+++ src/main/java/enactApp/enactAPI/data/translator/UserTranslator.java
@@ -5,6 +5,9 @@ import enactApp.enactAPI.data.model.User;
 import enactApp.enactAPI.data.service.UserService;
 import enactApp.enactAPI.web.models.UserView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserTranslator {
     public static UserView entityToView(final User user) {
         Boolean[] giIssues = UserService.getUserFrequentGiIssues(user.getId());
@@ -41,12 +44,44 @@ public class UserTranslator {
                 .build();
     }
 
+    public static ArrayList<UserView> entitiesToView(List<User> users) {
+        ArrayList<UserView> userViews = new ArrayList<>();
+        for (User user : users) {
+            Boolean[] giIssues = UserService.getUserFrequentGiIssues(user.getId());
+            Boolean[] cancerTreatment = UserService.getCancerTreatment(user.getId());
+            userViews.add(UserView.builder()
+                    .dateOfBirth(user.getDateOfBirth())
+                    .race(user.getRace())
+                    .ethnicity(user.getEthnicity())
+                    .gender((user.getGender()))
+                    .height(user.getHeight())
+                    .weight(user.getWeight())
+                    .fatPercent(user.getFatPercent())
+                    .proteinPercent(user.getProteinPercent())
+                    .carbohydratePercent(user.getCarbohydratePercent())
+                    .activityLevel(UserService.getActivityLevel(user.getActivityLevelId()))
+                    .abdominalPain(giIssues[0])
+                    .appetiteLoss(giIssues[1])
+                    .bloating(giIssues[2])
+                    .constipation(giIssues[3])
+                    .diarrhea(giIssues[4])
+                    .nausea(giIssues[5])
+                    .stomaProblems(giIssues[6])
+                    .colorectal(user.getColorectal())
+                    .stage(user.getStage())
+                    .diagnosisDate(user.getDiagnosisDate())
+                    .surgery(cancerTreatment[0])
+                    .chemo(cancerTreatment[1])
+                    .radiation(cancerTreatment[2])
+                    .other(cancerTreatment[3])
+                    .uncertain(cancerTreatment[4])
+                    .ostomy(cancerTreatment[5])
+                    .build());
+        }
+        return userViews;
 
 
-
-
-
-
+    }
 
 
 }
