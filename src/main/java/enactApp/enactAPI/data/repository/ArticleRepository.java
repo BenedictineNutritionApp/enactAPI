c1569d9d3080feca96b9f53854fddc7f6fe14c03
Brendamus <50035059+Brendamus@users.noreply.github.com>
2021-04-16 10:12:20 -0500

    Updated article controller, model, repo, and migration

--- src/main/java/enactApp/enactAPI/data/repository/ArticleRepository.java
+++ src/main/java/enactApp/enactAPI/data/repository/ArticleRepository.java
@@ -6,5 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
     List<Article> findAll();
+
+    List<Article> findAllByArticleAuthorLike(String searchString);
+
+    List<Article> findAllByArticleNameLike(String searchString);
+
+    List<Article> findAllByArticleSubjectLike(String searchString);
+    
+
 }
--- src/main/java/enactApp/enactAPI/web/controller/ArticleController.java
+++ src/main/java/enactApp/enactAPI/web/controller/ArticleController.java
@@ -1,11 +1,18 @@
 package enactApp.enactAPI.web.controller;
 
-import enactApp.enactAPI.data.model.Article;
+import enactApp.enactAPI.data.model.*;
 import enactApp.enactAPI.data.repository.ArticleRepository;
+import enactApp.enactAPI.web.payload.response.MessageResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -23,4 +30,50 @@ public class ArticleController {
         List<Article> articleList = articleRepository.findAll();
         return articleList;
     }
+
+    @PreAuthorize("hasRole('SUPER') or hasRole('MASTER')")
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@Valid @RequestParam("title") String title, @RequestParam("author")
+            String author, @RequestParam("topic") String topic, @RequestParam("file") MultipartFile file) throws IOException {
+        System.out.println("MADE IT TO ARTICLE UPLOAD");
+        System.out.println(file.getOriginalFilename());
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(topic);
+        System.out.println(file.getName());
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Article article = new Article(fileName, "author", "subject", "type", file.getBytes(), false);
+        article.setCreated(new Date());
+        article.setUpdated(new Date());
+        articleRepository.save(article);
+        return ResponseEntity.ok(new MessageResponse("Article uploaded successfully!"));
+    }
+
+    @PreAuthorize("hasRole('SUPER') or hasRole('MASTER')")
+    @PostMapping("/edit")
+    public ResponseEntity<?> upload(@Valid @RequestParam("id") Long id, @RequestParam("title") String title, @RequestParam("author")
+            String author, @RequestParam("topic") String topic, @RequestParam("file") MultipartFile file) throws IOException {
+        if (file.getContentType().equals("application/pdf")) {
+            System.out.println("MADE IT TO ARTICLE EDIT");
+            System.out.println(file.getOriginalFilename());
+            System.out.println(file.getContentType());
+            System.out.println(id);
+            System.out.println(title);
+            System.out.println(author);
+            System.out.println(topic);
+            System.out.println(file.getName());
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            Article article = new Article(fileName, "author", "subject", "type", file.getBytes(), false);
+            article.setCreated(new Date());
+            article.setUpdated(new Date());
+            articleRepository.save(article);
+            return ResponseEntity.ok(new MessageResponse("Article uploaded successfully!"));
+        } else {
+            System.out.println("BUMMERMAN");
+
+            return ResponseEntity.ok(new MessageResponse("Article uploaded successfully!"));
+        }
+    }
+//TODO CHANGE VISIBILITY
+    //TODO
 }
